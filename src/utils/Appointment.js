d4d94b41e058d795969ffb538fa982183bce4a63
faisal ridwan <faisal.ridwan55@gmail.com>
2018-12-04 10:55:42 +0700

    init template for lab 10

---
+++ src/utils/Appointment.js
@@ -0,0 +1,25 @@
+const cors = "https://cors-anywhere.herokuapp.com/";
+const baseUrl = "http://si-appointment.herokuapp.com/api";
+
+export const Appointment = {
+	getAllPasien() {
+		/** 
+		 * TODO: Fetch data all pasien
+		 * @return listAllPasien
+		 */
+	},
+	getDetailPasien(idPasien) {
+		/** 
+		 * TODO: Fetch detail data pasien
+		 * @param idPasien
+		 * @return detailPasien
+		 */
+	},
+	updateStatusPasien(requestBody) {
+		/** 
+		 * TODO: POST data baru pasien ke SI-Appointment
+		 * @param requestBody
+		 * @return responseRequest
+		 */
+	}
+}
